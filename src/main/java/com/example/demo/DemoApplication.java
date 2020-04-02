feee064b46f62de916bf09ff71d2641048b51475
吴仁彪 <42268027+GitHubFormerrybiao@users.noreply.github.com>
2020-04-02 22:37:25 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     //开发人员提交的代码
+    //项目经理提交的代码
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
