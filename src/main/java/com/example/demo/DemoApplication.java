fbbadc2593c72c5acdc0c42e4b2c27a7cc2f7361
merrybiao <907397772@qq.com>
2020-04-02 22:19:10 +0800

    开发人员提交的代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    //开发人员提交的代码
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
